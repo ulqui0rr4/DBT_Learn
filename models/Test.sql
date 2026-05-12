8d034525eb9ee7ca77677cdec58effde59a4ea6d
ulqui0rr4 <sayakroy199925@gmail.com>
2026-05-12 13:58:16 +0530

    date_dim

--- models/Test.sql
+++ models/Test.sql
@@ -1,1 +1,11 @@
-select * from {{source('demo', 'bikes')}}
+with CTE as (
+    select 
+    to_timestamp(START_TIME) as START_TIME,
+    Date(to_timestamp(START_TIME)) as start_date,
+    hour(to_timestamp(START_TIME)) as hour_time,
+    dayname(to_timestamp(START_TIME)) as week_days
+    
+     from {{source('demo', 'bikes')}}
+)
+
+select * from CTE
