e0ab99020045c558db7c3a9b2be3caa952a51705
ulqui0rr4 <sayakroy199925@gmail.com>
2026-05-12 20:30:04 +0530

    1 change

--- models/Test.sql
+++ models/Test.sql
@@ -1,11 +1,19 @@
+
+{{ config(materialized='table') }}
+
 with CTE as (
+
     select 
-    to_timestamp(START_TIME) as START_TIME,
-    Date(to_timestamp(START_TIME)) as start_date,
-    hour(to_timestamp(START_TIME)) as hour_time,
-    dayname(to_timestamp(START_TIME)) as week_days,
-    {{seasons('START_TIME')}} as season
-     from {{source('demo', 'bikes')}}
+        to_timestamp(START_TIME) as START_TIME
+
+
+        , date(to_timestamp(START_TIME)) as start_date
+        , hour(to_timestamp(START_TIME)) as hour_time
+
+
+    from {{ source('demo', 'bikes') }}
+
 )
+
 
 select * from CTE
